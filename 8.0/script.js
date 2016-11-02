cc647d4d89539c5605000922820eafc0d3a27814
Antonio Solano <jsolano91@gmail.com>
2016-11-02 18:06:36 -0400

    feat(almost done) only tooltip remaining

--- 8.0/script.js
+++ 8.0/script.js
@@ -35,17 +35,21 @@ var axisY = d3.axisLeft()
 
 //Line generator
 var lineGenerator = d3.line()
-    .x(function(d){return scaleX(d.year)})
-    .y(function(d){return scaleY(d.value)})
+    .x(function(d){ return scaleX(new Date(d.key)); })
+    .y(function(d){ return scaleY(new Date(d.averagePrice)); })
     .curve(d3.curveCardinal);
 
 d3.queue()
     .defer(d3.csv, '../data/bos-sfo-flight-fare.csv',parse)
     .await(function(err, data){
 
+        // console.log(data);
+
         //Mine the data to set the scales
-        scaleX.domain( d3.extent(data,function(d){return d.travelDate}) );
+        var ext = scaleX.domain( d3.extent(data,function(d){ return d.travelDate; }) );  //???
         scaleColor.domain( airlines.values() );
+
+        var _data = data;
 
         //Add buttons
         d3.select('.btn-group')
@@ -53,18 +57,19 @@ d3.queue()
             .data( airlines.values() )
             .enter()
             .append('a')
-            .html(function(d){return d})
+            .html(function(d){ return d; })
             .attr('href','#')
             .attr('class','btn btn-default')
             .style('color','white')
-            .style('background',function(d){return scaleColor(d)})
+            .style('background',function(d){ return scaleColor(d); })
             .style('border-color','white')
             .on('click',function(d){
-                //Hint: how do we filter flights for particular airlines?
-                //data.filter(...)
-
-                //How do we then update the dots?
+                var filteredData = _data.filter(function (el) {
+                    return(el.airline == d);
+                });
+                draw(filteredData);
             });
+
 
         //Draw axis
         plot.append('g').attr('class','axis axis-x')
@@ -73,25 +78,73 @@ d3.queue()
         plot.append('g').attr('class','axis axis-y')
             .call(axisY);
 
+        //Append path
+        plot.append('path')
+            .attr('class', 'average-line');
+
         draw(data);
 
     });
 
 function draw(rows){
     //IMPORTANT: data transformation
-    var flightsByTravelDate = d3.nest().key(function(d){return d.travelDate})
+
+    var test = rows.sort(function(a, b){
+        return (a.travelDate - b.travelDate);
+    });
+
+
+    var flightsByTravelDate = d3.nest().key(function(d){ return d.travelDate; })
         .entries(rows);
 
     flightsByTravelDate.forEach(function(day){
-       day.averagePrice = d3.mean(day.values, function(d){return d.price});
+       day.averagePrice = d3.mean(day.values, function(d){ return d.price; });
     });
 
-    console.log(flightsByTravelDate);
+    // why i couldn't sort my new array???
+
+    // console.log(flightsByTravelDate);
+    // console.log(rows);
 
     //Draw dots
+    //UPDATE
+    var node = plot.selectAll('.circle')
+        .data(rows,function(d){return d.id; });
 
+    //ENTER
+    var enter = node.enter()
+        .append('circle')
+        .attr('class', 'circle')
+        .attr('r', 0)
+        .attr('cx',function(d){ return scaleX(d.travelDate); })
+        .attr('cy', function(d){ return scaleY(d.price); })
+        .style('fill', function(d) { return scaleColor(d.airline); });
+        //mouse events go here...
+        
+    //UPDATE + ENTER
+    enter
+        .merge(node)
+        .transition()
+        .duration(1000)
+        .attr('r', 3)
+        .attr('cx',function(d){ return scaleX(d.travelDate); })
+        .attr('cy', function(d){ return scaleY(d.price); })
+        .style('fill', function(d) { return scaleColor(d.airline); })
+        .style('fill-opacity', 0.8);
+
+    //EXIT
+    node.exit().transition().attr("r", 0).remove();
 
     //Draw <path>
+    plot.select('.average-line')
+        .datum(flightsByTravelDate)
+        .transition()
+        .duration(1000)
+        .attr('d', lineGenerator)
+        .style('fill', 'none')
+        .style('stroke-width', '2px')
+        .style('stroke', function(array){ return scaleColor(array[0].values[0].airline); });
+
 }
 
 function parse(d){
@@ -106,5 +159,5 @@ function parse(d){
         travelDate: new Date(d.travelDate),
         duration: +d.duration,
         id: d.id
-    }
+    };
 }
